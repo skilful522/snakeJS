3b67eb76ecec4d5b78ba8de7b1e6d786cda44275
Stanislav <skilful522@gmail.com>
2020-06-02 12:25:12 +0300

    add actions sound and eat apple sound

---
+++ src/res/sounds/soundsPlaylist.js
@@ -0,0 +1,11 @@
+const soundsPlayList = {
+    eatFoodSounds: {
+        apple: 'src/res/sounds/food/eat-sound.m4a',
+    },
+    actionsSounds: {
+        snakeIncreased: 'src/res/sounds/gift/snake-increased.m4a',
+        snakeDecreased: 'src/res/sounds/gift/snake-decreased.m4a',
+    },
+};
+
+export default soundsPlayList;
