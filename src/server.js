c16646372780552dfcf55e3110c78c0c8786929e
Stanislav <skilful522@gmail.com>
2020-06-03 13:29:49 +0300

    change server

---
+++ src/server.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(function(request, response) {
+    const filePath = `.${request.url}` === './' ? './index.html' : `.${request.url}`;
+    const extname = String(path.extname(filePath)).toLowerCase();
+
+    let contentType = 'text/html';
+    const mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+    };
+
+    contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if (error.code === 'ENOENT') {
+                response.writeHead(404, { 'Content-Type': contentType });
+                response.end();
+            } else {
+                response.writeHead(500);
+                response.end(`Sorry, check with the site admin for error: ${error.code} ..\n`);
+                response.end();
+            }
+        } else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+});
+
+server.listen(port);
